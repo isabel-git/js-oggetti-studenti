7ed96bae0b660e11817dc4f31495f661384de575
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-09 16:05:12 +0100

    ex 1 completato

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-$(document).ready(function () {
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+// - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// - Dare la possibilità all’utente attraverso 3 prompt di
+// aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. 
 
+// PRIMO EX
+var studente = {
+  "nome": 'Lisa',
+  "cognome": 'Simpson',
+  "eta": 8
+}
 
-
-
-});
+// stampo proprietà
+for (var key in studente) {
+  
+  console.log((key + ": " + studente[key]));
+}
